c3dfcbbb999887eaa850e0a41ba750bf6402947c
Emilio <emilio.d.duarte.miranda@intel.com>
2016-11-23 09:39:41 -0600

    first commit

---
+++ Agilysys/Agilysys.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace AgilysysProject
+{
+    public class Agilysys
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 1, 2, 3 };
+            var clean_array = remove_duplicates(array);
+
+            foreach (var item in clean_array)
+                Console.WriteLine(item);
+            Console.ReadLine();
+        }
+
+
+        public static int[] remove_duplicates(int[] array_of_entries) {
+            var distinct_array = array_of_entries.Distinct().ToArray();
+            return distinct_array;
+        }
+    }
+}
---
+++ TestingRemovingDuplicates/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AgilysysProject;
+
+namespace TestingRemovingDuplicates
+{
+    [TestClass]
+    public class RemovingDuplicatesFromArrayTest
+    {
+
+        [TestMethod]
+        public void TestRemoveDuplicatesWithEmptyArrayShouldReturnAnEmptyArray()
+        {
+            int[] array = { };
+            int[] expected_array = { };
+
+            var clean_array = Agilysys.remove_duplicates(array);
+
+            CollectionAssert.AreEqual(expected_array,clean_array);
+        }
+
+        [TestMethod]
+        public void TestRemoveDuplicatesWithSixElementsShouldReturnThreeUniqueValues()
+        {
+            int[] array = {1,2,3,1,2,3};
+            int[] expected_array = {1,2,3};
+
+            var clean_array = Agilysys.remove_duplicates(array);
+
+            CollectionAssert.AreEqual(expected_array, clean_array);
+        }
+
+        [TestMethod]
+        public void TestRemoveDuplicatesWithSixElementsShouldReturnSixUniqueValues()
+        {
+            int[] array = { 1, -1, 2, -2, 3, -3 };
+            int[] expected_array = { 1, -1, 2, -2, 3, -3 };
+
+            var clean_array = Agilysys.remove_duplicates(array);
+
+            CollectionAssert.AreEqual(expected_array, clean_array);
+        }
+    }
+}
